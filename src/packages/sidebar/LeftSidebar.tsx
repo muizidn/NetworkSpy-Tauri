faa237e851fe4f533bde9660dcf7458a164acbc3
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2024-07-21 14:43:55 +0700

    Add search in Left Sidebar

--- src/packages/sidebar/LeftSidebar.tsx
+++ src/packages/sidebar/LeftSidebar.tsx
@@ -122,26 +122,38 @@ export const LeftSidebar = () => {
   const allTrees = [app, domain];
 
   return (
-    <div className="p-2 bg-[#23262a] border border-black h-full w-full flex flex-col space-y-4 items-start overflow-scroll">
-      <div className="flex flex-col space-y-2 items-start w-full">
-        <h2 className="font-bold text-lg text-white">Favorites</h2>
-        {favoriteTrees.map((e) => (
-          <SidebarTreeView
-            key={e.name}
-            node={e}
-            onClick={(name) => onClickNode(name)}
-          />
-        ))}
+    <div className="bg-[#23262a] border-r border-black h-full w-full flex flex-col space-y-4">
+      <div className="p-2 flex flex-col space-y-4 h-full w-full  items-start overflow-scroll">
+        <div className="flex flex-col space-y-2 items-start w-full">
+          <h2 className="font-bold text-lg text-white">Favorites</h2>
+          {favoriteTrees.map((e) => (
+            <SidebarTreeView
+              key={e.name}
+              node={e}
+              onClick={(name) => onClickNode(name)}
+            />
+          ))}
+        </div>
+        <div className="flex flex-col space-y-2 items-start w-full">
+          <h2 className="font-bold text-lg text-white">All</h2>
+          {allTrees.map((e) => (
+            <SidebarTreeView
+              key={e.name}
+              node={e}
+              onClick={(name) => onClickNode(name)}
+            />
+          ))}
+        </div>
       </div>
-      <div className="flex flex-col space-y-2 items-start w-full">
-        <h2 className="font-bold text-lg text-white">All</h2>
-        {allTrees.map((e) => (
-          <SidebarTreeView
-            key={e.name}
-            node={e}
-            onClick={(name) => onClickNode(name)}
-          />
-        ))}
+      <div className="p-2 flex space-x-2 w-full border-t border-black">
+        <button className="btn btn-xs bg-[#474b49] rounded text-white">
+          +
+        </button>
+        <input
+          type="text"
+          className="input input-xs flex-grow rounded bg-[#474b49] w-full"
+          placeholder="Search"
+        />
       </div>
     </div>
   );
