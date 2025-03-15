b91d5be1c2f3e0a7b172ec7684ae8a9039430e29
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2025-03-15 09:49:04 +0700

    Add RightSidebar story

---
+++ src/stories/app/components/RightSidebar.stories.tsx
@@ -0,0 +1,23 @@
+import { RightSidebar } from '@src/packages/sidebar/RightSidebar';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+
+const meta = {
+  title: 'App/Components/RightSidebar',
+  component: RightSidebar,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'padded',
+  },
+  args: {
+  },
+} satisfies Meta<typeof RightSidebar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+  },
+};
